00c61fa87dc78427f56a082cf4892a8a429dbed6
Deepthi <deepthip18016@gmail.com>
2025-12-10 11:08:42 +0530

    Update word_cloud_generator_app.py

--- word_cloud_generator_app.py
+++ word_cloud_generator_app.py
@@ -43,7 +43,7 @@ if uploaded_file is not None:
     clean_text = preprocess_text(text)
 
     # Tokenize
-    words = word_tokenize(clean_text)
+    words = word_tokenize(clean, preserve_line=True)
 
     # Remove stopwords
     stop_words = set(stopwords.words("english"))
